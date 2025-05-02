b3cedc97d1ef1b6451197337ba80de1970e3a6bc
Zhicheng Gong <109425to@gmail.com>
2025-05-02 22:05:45 +0800

    just some test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-2 grow p-8">
       <Cog size={30}></Cog>
-      <h1 className="text-3xl mt-2">目前網站仍在建置中</h1>
+      <h1 className="text-3xl mt-3">目前網站仍在建置中</h1>
       <Link
         href={"./events/baseball"}
         className="p-3 px-5 shadow-2xl shadow-zinc-300 rounded-full bg-zinc-900 text-white hover:opacity-80 mt-5"
